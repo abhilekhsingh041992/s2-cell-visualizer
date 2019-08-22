f1a2d921e2560185dc4b31365adee8a29f44c7b9
apple <apple@apples-MacBook-Pro.local>
2019-08-22 22:33:38 +0530

    Updates

---
+++ precache-manifest.9288c862639439588544476856622d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822788d13d83b36453971e5261a6cde6",
+    "url": "/s2-cell-visualizer/index.html"
+  },
+  {
+    "revision": "46397ba83f6dcd48d44e",
+    "url": "/s2-cell-visualizer/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c6791e51c56eee575503",
+    "url": "/s2-cell-visualizer/static/js/2.3f1671b2.chunk.js"
+  },
+  {
+    "revision": "46397ba83f6dcd48d44e",
+    "url": "/s2-cell-visualizer/static/js/main.55109000.chunk.js"
+  },
+  {
+    "revision": "6532ff713f32b11a083a",
+    "url": "/s2-cell-visualizer/static/js/runtime~main.b35886f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/s2-cell-visualizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
